a78033daa84de8ddea7c7009efd42f3ffb37ece8
League Student <league@iMac-87.attlocal.net>
2019-06-13 18:30:06 -0700

    Are_You_Happy :smile:

--- src/_04_are_you_happy/Are_you_happy.java
+++ src/_04_are_you_happy/Are_you_happy.java
@@ -1,5 +1,21 @@
 package _04_are_you_happy;
 
-public class Are_you_happy {
+import javax.swing.JOptionPane;
 
+public class Are_you_happy {
+public static void main(String[] args) {
+	String happy = JOptionPane.showInputDialog( "Are you happy?");
+	if (happy.equalsIgnoreCase("yes")) {
+		JOptionPane.showMessageDialog(null, "Keep doing what you are doing");
+	}
+	else {
+		String sure = JOptionPane.showInputDialog("Do you wanna be happy?");
+	if (sure.equalsIgnoreCase("yes")) {
+		JOptionPane.showMessageDialog(null, "Then change something");
+	}
+	else {
+JOptionPane.showMessageDialog(null, "Keep doing what you are doing");
+	}
+	}
+}
 }
